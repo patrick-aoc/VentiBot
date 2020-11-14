c212ebe4c162f754cef6be964d136b5e8da85ae4
Patrick Angelo Ocampo <patrick.ocampo@mail.utoronto.ca>
2020-11-14 18:39:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await message.channel.send("uwu")
+        #await client.send_message(message.channel, message.content[::-1])
+
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(token)
