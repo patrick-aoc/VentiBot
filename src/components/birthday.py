7391601aeb1c00547d476c2054027bb0fadf0a1c
Patrick Angelo Ocampo <patrick.ocampo@mail.utoronto.ca>
2021-03-10 03:51:53 +0000

    add list cmd

--- src/components/birthday.py
+++ src/components/birthday.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 import datetime
@@ -24,20 +25,30 @@ class Birthday(commands.Cog):
 
         if cmd == "add":
           if len(args) != 3:
-            await ctx.send("Invalid number of arguments specified for 'add'. For more information, please specify 'Venti-san! birthday help'")
+            await ctx.send("Invalid number of arguments specified for 'add'. For more information, please specify 'Venti-san! birthday ?'")
           else:
             await self._add_bd(ctx, args[1], args[2])
+        elif cmd == "list":
+          await self._get_bds(ctx)
+        elif cmd == "help":
+          await self._help(ctx)
         elif cmd == "list":
           await self._get_bds(ctx)
         else:
           await ctx.send("Invalid command '{c}' specified".format(c=cmd))    
     
+    async def _help(self, ctx: commands.Context):
+        await ctx.send("Not yet implemented")
+
     async def _add_bd(self, ctx: commands.Context, birthday, celebrant):
       if "/" in birthday or "-" in birthday:
         print("owo")
         delim = "/" if "/" in birthday else "-"
 
         try:
+
+          # Birthdays must be in the format:
+          # MM-DD-YYYY or MM/DD/YY
           year = int(birthday.split(delim)[2])
           month = int(birthday.split(delim)[0])
           day = int(birthday.split(delim)[1])
@@ -49,30 +60,34 @@ class Birthday(commands.Cog):
             uname = str(mem).split("#")[0]
             uid = str(mem).split("#")[1]
 
-            if celebrant in str(mem):
-              cb = str(mem)
+            if celebrant in uname:
+              cb = mem
               break
 
           self.firebase_db.push(
               {
-                "celebrant": cb,
+                "celebrant": str(cb),
                 "birthday": birthday
               }
-            )
-          await ctx.send("Birthday has been added for <@{}> ... {}".format(cb, bdate))
+          )
+          await ctx.send("Birthday has been added for <@{}> ... {}".format(cb.id, bdate))
         except Exception as e:
           print(e)
       else:
         pass
 
-    
     async def _get_bd(self, ctx: commands.Context, celebrant):
       pass
 
     async def _get_bds(self, ctx: commands.Context):
-      pass
+        embed = (discord.Embed(title='Birthdays',
+                               color=discord.Color.blurple()))
+        birthdays = ""
 
+        for key, value in (self.firebase_db.get()).items():
+            birthdays += "{} ... {} \n".format(value["celebrant"], value["birthday"])
 
-    
+        embed.add_field(name="Celebrant ... Birthday", value=birthdays)
+        await ctx.send(embed=embed)
     
       
